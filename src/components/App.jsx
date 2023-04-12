6ef978ff83072e649289e47f2e8d7fe8637da704
Alexander-Solonar <Alexander-Solonar_solo991@ukr.net>
2023-04-12 20:20:33 +0300

    add App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,55 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import userContacts from '../data/contacts.json';
+import ContactForm from './contactForm';
+import ContactList from './contactList';
+import Filter from './filter';
+
+export class App extends Component {
+  state = {
+    contacts: userContacts,
+    filter: '',
+  };
+
+  addContacts = data => {
+    const nameUser = this.state.contacts.map(({ name }) => name);
+    if (nameUser.includes(data.name)) {
+      alert(`${data.name} is already in contacts.`);
+      return;
+    }
+    this.setState(({ contacts }) => ({
+      contacts: [data, ...contacts],
+    }));
+  };
+
+  filterChange = event => {
+    this.setState({ filter: event.target.value });
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+
+    const normalizedFilter = filter.toLowerCase();
+    const visibleContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm addContacts={this.addContacts} />
+        <h2>Contacts</h2>
+        <Filter value={this.state.filter} onfilterChange={this.filterChange} />
+        <ContactList
+          data={visibleContacts}
+          onDeleteContact={this.deleteContact}
+        />
+      </div>
+    );
+  }
+}
